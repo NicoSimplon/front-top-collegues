2f7e200f5dde37523b51191a3940e4db1e1a865a
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-05-13 21:54:25 +0200

    Génération des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { ClassementComponent } from './classement/classement.component';
+import { VoteComponent } from './vote/vote.component';
+import { MenuComponent } from './menu/menu.component';
+import { ParticipantComponent } from './participant/participant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthentificationComponent,
+    ClassementComponent,
+    VoteComponent,
+    MenuComponent,
+    ParticipantComponent
   ],
   imports: [
     BrowserModule
